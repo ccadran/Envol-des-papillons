12001197dc3559b212c5875732515fe3ff3dddc8
ccadran <clariocadran33@gmail.com>
2023-06-22 10:07:54 +0200

    navigation global done

--- frontend/src/components/shared/NavigationGlobal.jsx
+++ frontend/src/components/shared/NavigationGlobal.jsx
@@ -1,10 +1,47 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import PapillonLogo from "./PapillonLogo";
 
 const NavigationGlobal = () => {
   return (
-    <div>
-      <h1>Navigation établissement</h1>
-    </div>
+    <nav className="nav-global">
+      <div className="nav-home">
+        <NavLink to="/">
+          <PapillonLogo />
+          <li>Acceuil</li>
+        </NavLink>
+      </div>
+      <div className="nav-links">
+        <ul>
+          <NavLink
+            to="/ecole/presentation"
+            className="nav-link"
+            activeclassname="active"
+          >
+            <li>L'école</li>
+          </NavLink>
+          <NavLink
+            to="/college/presentation"
+            className="nav-link"
+            activeclassname="active"
+          >
+            <li>Le collège</li>
+          </NavLink>
+          <NavLink to="/blog" className="nav-link" activeclassname="active">
+            <li>Le blog</li>
+          </NavLink>
+          <NavLink to="/FAQ" className="nav-link" activeclassname="active">
+            <li>FAQ</li>
+          </NavLink>
+          <NavLink to="/contact" className="nav-link" activeclassname="active">
+            <li>Contactez-nous</li>
+          </NavLink>
+          <NavLink to="/parents" className="nav-link" activeclassname="active">
+            <li>Parents d'élèves</li>
+          </NavLink>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
