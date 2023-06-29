7ae61a5da4e6b994959c53f91bcdcf8c5aba33ad
ccadran <clariocadran33@gmail.com>
2023-06-29 16:38:30 +0200

    add a new question fot the faq

--- frontend/src/components/admin/FAQ/FormFAQQuestion.jsx
+++ frontend/src/components/admin/FAQ/FormFAQQuestion.jsx
@@ -6,12 +6,15 @@ import { set } from "mongoose";
 
 const FormFAQQuestion = () => {
   const navigate = useNavigate();
-  const [selectedOption, setSelectedOption] = useState("");
+  const [selectedEtablissement, setSelectedEtablissement] = useState("");
   const [sectionTitles, setSectionTitles] = useState([]);
+  const [selectedSection, setSelectedSection] = useState("");
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
 
-  const handleOptionChange = (e) => {
+  const handleEtablissementChange = (e) => {
     const selectedValue = e.target.value;
-    setSelectedOption(selectedValue);
+    setSelectedEtablissement(selectedValue);
 
     if (selectedValue) {
       axios
@@ -26,32 +29,105 @@ const FormFAQQuestion = () => {
     }
   };
 
+  const handleSectionChange = (e) => {
+    const selectedValue = e.target.value;
+    setSelectedSection(selectedValue);
+  };
+
+  const handleQuestionChange = (e) => {
+    const value = e.target.value;
+    setQuestion(value);
+  };
+
+  const handleAnswerChange = (e) => {
+    const value = e.target.value;
+    setAnswer(value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(selectedSection);
+    // Créez un objet contenant les données du formulaire
+    const formData = {
+      section_id: selectedSection,
+      question: question,
+      question_answer: answer,
+    };
+
+    // Envoyez les données à votre backend en utilisant la méthode POST d'axios
+    axios
+      .post(
+        `http://localhost:5001/faqQuestion${selectedEtablissement}`,
+        formData
+      )
+      .then((res) => {
+        // Traitez la réponse du backend si nécessaire
+        console.log(res.data);
+
+        // Réinitialisez les valeurs du formulaire
+        setSelectedEtablissement("");
+        setSelectedSection("");
+        setQuestion("");
+        setAnswer("");
+
+        // Naviguez vers une autre page si nécessaire
+        navigate("/admin/FAQ"); // Remplacez "/success" par le chemin de la page de succès de votre choix
+      })
+      .catch((error) => {
+        console.error("Erreur lors de l'envoi des données :", error);
+      });
+  };
+
   return (
     <div className="form-faq-question">
       <div className="etablissement-select">
         <select
-          id="SelectOption"
-          value={selectedOption}
-          onChange={handleOptionChange}
+          id="SelectEtablissement"
+          value={selectedEtablissement}
+          onChange={handleEtablissementChange}
         >
-          <option value="">Sélectionner l'établissement </option>
+          <option value="">Sélectionner l'établissement</option>
           <option value="College">Collège</option>
           <option value="School">École</option>
         </select>
       </div>
-      {selectedOption ? (
-        <form action="">
-          <select id="select-section">
+      {selectedEtablissement ? (
+        <form onSubmit={handleSubmit}>
+          <select
+            id="select-section"
+            onChange={handleSectionChange}
+            value={selectedSection}
+          >
+            <option value="">Sélectionner une section</option>
             {sectionTitles.map((sectionTitle) => (
               <option value={sectionTitle._id} key={sectionTitle._id}>
                 {sectionTitle.section_title}
               </option>
             ))}
           </select>
+
+          <div className="question">
+            <label htmlFor="question">La question :</label>
+            <input
+              type="text"
+              id="question"
+              value={question}
+              onChange={handleQuestionChange}
+            />
+          </div>
+          <div className="answer">
+            <label htmlFor="answer">La réponse :</label>
+            <textarea
+              id="answer"
+              value={answer}
+              onChange={handleAnswerChange}
+            ></textarea>
+          </div>
+          <button type="submit">Envoyer</button>
         </form>
       ) : (
         <p>
-          Veuillez séléctionner l'établissement pour lequel vous voulez ajouter
+          Veuillez sélectionner l'établissement pour lequel vous voulez ajouter
           une question
         </p>
       )}
