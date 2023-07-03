18f05c6e9d00eb2d6ed2094c38aa2e4a4dc4365f
ccadran <clariocadran33@gmail.com>
2023-07-03 13:22:01 +0200

    start settings.scss

--- frontend/src/pages/admin/Evenements.jsx
+++ frontend/src/pages/admin/Evenements.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import NavigationAdmin from "../../components/admin/NavigationAdmin";
+import H1HeroText from "../../components/shared/H1HeroText";
 
 const Evenements = () => {
   return (
     <>
       <NavigationAdmin />
+      <H1HeroText
+        h1="Evenements"
+        text="Gérez les évènements de votre établissement"
+      />
+      <h1>API Google Agenda</h1>
     </>
   );
 };
