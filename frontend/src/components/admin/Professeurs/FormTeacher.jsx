d7ac1b5a28f5c7dd7e47310bc4eb11e91b5181c5
ccadran <clariocadran33@gmail.com>
2023-07-04 19:09:13 +0200

    add new teacher

--- frontend/src/components/admin/Professeurs/FormTeacher.jsx
+++ frontend/src/components/admin/Professeurs/FormTeacher.jsx
@@ -1,12 +1,99 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const FormTeacher = () => {
+  const [selectedOption, setSelectedOption] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [classe, setClasse] = useState("");
+  const [poste, setPoste] = useState("");
+  const [formation, setFormation] = useState("");
+  const navigate = useNavigate();
+  const handleOptionChange = (e) => {
+    setSelectedOption(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newTeacher = {
+      firstName: firstName,
+      lastName: lastName,
+      classe: classe,
+      poste: poste,
+      formation: formation,
+      etablissement: selectedOption,
+    };
+
+    axios
+      .post("http://localhost:5001/teacher", newTeacher)
+      .then((response) => {
+        navigate("/admin/teachers");
+        console.log("Nouveau professeur ajouté avec succès !");
+        // Réinitialiser les champs du formulaire
+        setFirstName("");
+        setLastName("");
+        setClasse("");
+        setPoste("");
+        setFormation("");
+        setSelectedOption("");
+      })
+      .catch((error) => {
+        console.error("Erreur lors de l'ajout du professeur :", error);
+      });
+  };
+
   return (
-    <div>
-      <h1>Form teacher</h1>
+    <div className="form-teacher">
+      <form onSubmit={handleSubmit}>
+        <div className="etablissement-select">
+          <select
+            id="SelectOption"
+            value={selectedOption}
+            onChange={handleOptionChange}
+          >
+            <option value="">Sélectionner l'établissement</option>
+            <option value="collège">Collège</option>
+            <option value="école">École</option>
+          </select>
+        </div>
+
+        <input
+          type="text"
+          placeholder="Prénom"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Nom"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Classe"
+          value={classe}
+          onChange={(e) => setClasse(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Poste"
+          value={poste}
+          onChange={(e) => setPoste(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Formation"
+          value={formation}
+          onChange={(e) => setFormation(e.target.value)}
+        />
+
+        <button type="submit">Ajouter</button>
+      </form>
     </div>
   );
 };
 
 export default FormTeacher;
-<h1>Form teacher</h1>;
--- frontend/src/pages/admin/NewTeacher.jsx
+++ frontend/src/pages/admin/NewTeacher.jsx
@@ -1,10 +1,22 @@
+import NavigationAdmin from "components/admin/NavigationAdmin";
+import FormTeacher from "components/admin/Professeurs/FormTeacher";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NewTeacher = () => {
   return (
-    <div>
-      <h1>Ajouter un prof</h1>
-    </div>
+    <>
+      <NavigationAdmin />
+      <Link to="/admin/teachers">Retour</Link>
+      <div className="intro">
+        <h4>Ajouter un professeur</h4>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque
+          adipisci iste perspiciatis pariatur, beatae corporis.
+        </p>
+      </div>
+      <FormTeacher />
+    </>
   );
 };
 
