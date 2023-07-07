5c688b864f402d85112935879e85ff75612b03fb
ccadran <clariocadran33@gmail.com>
2023-07-07 17:05:33 +0200

    blogArticle style

--- frontend/src/pages/etablissement/BlogArticle.jsx
+++ frontend/src/pages/etablissement/BlogArticle.jsx
@@ -8,6 +8,7 @@ import NavigationGlobal from "../../components/shared/NavigationGlobal";
 import { useLocation } from "react-router-dom";
 import NavigationAdmin from "../../components/admin/NavigationAdmin";
 import Tags from "components/parents/Actualités/Tags";
+import chevron from "assets/logos/chevron.svg";
 import "styles/etablissement/Blog/_blogArticle.scss";
 
 const BlogArticle = () => {
@@ -17,6 +18,14 @@ const BlogArticle = () => {
   const isRootPath = location.pathname.includes("admin");
   const { id } = useParams();
   const navigate = useNavigate();
+  const dateFormated = (date) => {
+    return new Date(date).toLocaleDateString("fr-FR", {
+      weekday: "long",
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+    });
+  };
 
   useEffect(() => {
     axios.get(`http://localhost:5001/blog/${id}`).then((res) => {
@@ -141,7 +150,8 @@ const BlogArticle = () => {
           <main>
             <section className="blog-article">
               <div className="back">
-                <Link to="/etablissement/blog">retour</Link>
+                <img src={chevron} alt="" />
+                <Link to="/etablissement/blog">Retour</Link>
               </div>
               <div className="article-container">
                 <div className="article-header">
@@ -156,20 +166,20 @@ const BlogArticle = () => {
                 </div>
                 <div className="article-content">
                   <div className="article-text">
-                    <p>{blogArticle.accroche} </p>
+                    <p id="intro">{blogArticle.introduction} </p>
                     <h4>{blogArticle.subTitle1} </h4>
                     <p>{blogArticle.content1} </p>
                     <h4>{blogArticle.subTitle2} </h4>
                     <p>{blogArticle.content2} </p>
-                    <p>{blogArticle.conclusion} </p>
+                    <p id="conclusion">{blogArticle.conclusion} </p>
                   </div>
                   <div className="article-images">{/* //Les images */}</div>
                 </div>
-                <div className="artcile-author">
+                <div className="article-author">
                   <PapillonLogo />
                   <div className="author-info">
                     <h5>{blogArticle.author} </h5>
-                    <p> {blogArticle.date}</p>
+                    <p> {dateFormated(blogArticle.createdAt)}</p>
                   </div>
                 </div>
               </div>
