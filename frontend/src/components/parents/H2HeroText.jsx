dada9b8e5413ec63dce4e4cb1478a9d4a81ac235
ccadran <clariocadran33@gmail.com>
2023-07-11 11:36:08 +0200

    parents hero

--- frontend/src/components/parents/H2HeroText.jsx
+++ frontend/src/components/parents/H2HeroText.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import "styles/parents/shared/_H2HeroText.scss";
 
 const H2HeroText = ({ h2, text }) => {
   return (
-    <div>
+    <div className="h2-hero-text">
       <h2>{h2}</h2>
       <p>{text}</p>
     </div>
