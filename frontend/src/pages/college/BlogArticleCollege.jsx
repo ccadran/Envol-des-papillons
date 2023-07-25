95b2671f40a313a2352bb94280c952ae053a3c0a
ccadran <clariocadran33@gmail.com>
2023-07-25 16:19:46 +0200

    delete unusual code

--- frontend/src/pages/college/BlogArticleCollege.jsx
+++ frontend/src/pages/college/BlogArticleCollege.jsx
@@ -20,12 +20,8 @@ import CarousselArticle from "components/shared/CarousselArticle";
 
 const BlogArticleCollege = () => {
   const [blogArticle, setBlogArticle] = useState([]);
-  const [updatedBlogArticle, setUpdatedBlogArticle] = useState({
-    tags: [],
-  });
 
   const location = useLocation();
-  const isRootPath = location.pathname.includes("admin");
   let backUrl = "";
   if (location.pathname.includes("etablissement")) {
     backUrl = "/etablissement/blog";
@@ -48,107 +44,8 @@ const BlogArticleCollege = () => {
   useEffect(() => {
     axios.get(`http://localhost:5001/blog/${id}`).then((res) => {
       setBlogArticle(res.data);
-      setUpdatedBlogArticle(res.data);
     });
   }, [id]);
-
-  const handleInputChange = (e) => {
-    const { name, value, files } = e.target;
-
-    if (name === "mainImg") {
-      // If it's a file input for mainImg, use the first selected file
-      const file0 = files[0];
-      setUpdatedBlogArticle((prevArticle) => ({
-        ...prevArticle,
-        mainImg: file0,
-      }));
-    } else if (name === "illustrations") {
-      // If it's a file input for illustrations, use all selected files
-      const fileList = Array.from(files); // Convert FileList to an array
-      setUpdatedBlogArticle((prevArticle) => ({
-        ...prevArticle,
-        illustrations: fileList,
-      }));
-    } else {
-      // For other inputs, set the value directly
-      setUpdatedBlogArticle((prevArticle) => ({
-        ...prevArticle,
-        [name]: value,
-      }));
-    }
-    // console.log("Updated article:", updatedBlogArticle);
-  };
-
-  const handleTagClick = (tag) => {
-    setUpdatedBlogArticle((prevArticle) => {
-      const updatedTags = prevArticle.tags.includes(tag)
-        ? prevArticle.tags.filter((t) => t !== tag)
-        : [...prevArticle.tags, tag];
-
-      console.log("Updated tags:", updatedTags);
-
-      return {
-        ...prevArticle,
-        tags: updatedTags,
-      };
-    });
-  };
-
-  const handleUpdateArticle = () => {
-    const formData = new FormData();
-
-    formData.append("title", updatedBlogArticle.title);
-    formData.append("accroche", updatedBlogArticle.accroche);
-    formData.append("introduction", updatedBlogArticle.introduction);
-    formData.append("subTitle1", updatedBlogArticle.subTitle1);
-    formData.append("content1", updatedBlogArticle.content1);
-    formData.append("subTitle2", updatedBlogArticle.subTitle2);
-    formData.append("content2", updatedBlogArticle.content2);
-    formData.append("author", blogArticle.author);
-
-    // Vérifiez si une nouvelle image principale a été sélectionnée
-    if (updatedBlogArticle.mainImg instanceof File) {
-      formData.append("mainImg", updatedBlogArticle.mainImg);
-    } else {
-      // Si aucune nouvelle image n'a été sélectionnée,
-      // conserver l'BACKURL de l'ancienne image principale dans le FormData
-      formData.append("mainImg", blogArticle.mainImg);
-    }
-    // Vérifiez si de nouvelles illustrations ont été sélectionnées
-    if (
-      Array.isArray(updatedBlogArticle.illustrations) &&
-      updatedBlogArticle.illustrations.length > 0
-    ) {
-      updatedBlogArticle.illustrations.forEach((illustration) => {
-        formData.append("illustrations", illustration);
-      });
-    } else {
-      // Si aucune nouvelle illustration n'a été sélectionnée,
-      // conserver les BACKURLs des illustrations existantes dans le FormData
-      blogArticle.illustrations.forEach((illustration) => {
-        formData.append("illustrations", illustration);
-      });
-    }
-    axios
-      .put(`http://localhost:5001/blog/${id}`, formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      })
-      .then((res) => {
-        // Traitement après la mise à jour réussie
-        console.log("Article mis à jour avec succès !");
-        navigate(`/admin/blog`);
-      })
-      .catch((error) => {
-        // Traitement en cas d'erreur lors de la mise à jour
-        console.error("Erreur lors de la mise à jour de l'article :", error);
-      });
-  };
-
-  if (!blogArticle) {
-    return <div>Loading...</div>;
-  }
 
   return (
     <>
