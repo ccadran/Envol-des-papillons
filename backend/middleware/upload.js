b8c490d1dc2ff6ee02e4d77ae134a7c3f0c8e1ac
ccadran <clariocadran33@gmail.com>
2023-08-22 13:47:18 +0200

    change multer upload path

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -3,7 +3,8 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "frontend/public/uploads/");
+    const uploadPath = path.join(__dirname, "../../frontend/public/uploads/");
+    cb(null, uploadPath);
   },
   filename: function (req, file, cb) {
     let ext = path.extname(file.originalname);
